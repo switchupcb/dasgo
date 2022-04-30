67f96f232baeaf0cb3ae9e5f033607d732d99eab
switchupcb <contact@switchupcb.com>
2022-04-30 01:31:20 -0500

    add requests and responses
    Co-Authored-By: t-rog <74488354+t-rog@users.noreply.github.com>

---
+++ dasgo/responses.go
@@ -0,0 +1,68 @@
+package dasgo
+
+import "time"
+
+// Get Gateway Response
+// https://discord.com/developers/docs/topics/gateway#get-gateway-example-response
+type GetGatewayResponse struct {
+	URL string `json:"url,omitempty"`
+}
+
+// Get Gateway Bot Response
+// https://discord.com/developers/docs/topics/gateway#get-gateway-example-response
+type GetGatewayBotResponse struct {
+	URL               string `json:"url,omitempty"`
+	Shards            *int   `json:"shards,omitempty"`
+	SessionStartLimit struct {
+		Total      uint `json:"total"`
+		Remaining  uint `json:"remaining"`
+		ResetAfter uint `json:"reset_after"`
+	} `json:"session_start_limit"`
+}
+
+// Current Authorization Information Response Structure
+// https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
+type CurrentAuthorizationInformationResponse struct {
+	Application *Application `json:"application"`
+	Scopes      []*int       `json:"scopes"`
+	Expires     *time.Time   `json:"expires"`
+	User        *User        `json:"user"`
+}
+
+// List Active Threads Response Body
+// https://discord.com/developers/docs/resources/channel#list-active-threads-response-body
+type ListActiveThreadsResponse struct {
+	Threads []*Channel      `json:"threads"`
+	Members []*ThreadMember `json:"members"`
+	HasMore bool            `json:"has_more"`
+}
+
+// List Public Archived Threads Response Body
+// https://discord.com/developers/docs/resources/channel#list-active-threads-response-body
+type ListPublicArchivedThreadsResponse struct {
+	Threads []*Channel      `json:"threads"`
+	Members []*ThreadMember `json:"members"`
+	HasMore bool            `json:"has_more"`
+}
+
+// List Private Archived Threads Response Body
+// https://discord.com/developers/docs/resources/channel#list-active-threads-response-body
+type ListPrivateArchivedThreadsResponse struct {
+	Threads []*Channel      `json:"threads"`
+	Members []*ThreadMember `json:"members"`
+	HasMore bool            `json:"has_more"`
+}
+
+// List Joined Private Archived Threads Response Body
+// https://discord.com/developers/docs/resources/channel#list-active-threads-response-body
+type ListJoinedPrivateArchivedThreadsResponse struct {
+	Threads []*Channel      `json:"threads"`
+	Members []*ThreadMember `json:"members"`
+	HasMore bool            `json:"has_more"`
+}
+
+// Modify Current User Nick Response
+// https://discord.com/developers/docs/topics/gateway#get-gateway-example-response
+type ModifyCurrentUserNickResponse struct {
+	Nick *string `json:"nick,omitempty"`
+}
