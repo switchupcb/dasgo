70cbb976ec3e26783bd75f2d1af9ba33cd22f84d
switchupcb <contact@switchupcb.com>
2022-05-16 19:01:45 -0500

    fix get gateway bot

--- dasgo/requests.go
+++ dasgo/requests.go
@@ -664,11 +664,7 @@ type GetGateway struct{}
 // Get Gateway Bot
 // GET /gateway/bot
 // https://discord.com/developers/docs/topics/gateway#get-gateway-bot
-type GetGatewayBot struct {
-	URL               string `json:"url,omitempty"`
-	Shards            int    `json:"shards,omitempty"`
-	SessionStartLimit int    `json:"session_start_limit,omitempty"`
-}
+type GetGatewayBot struct{}
 
 // Create Guild
 // POST /guilds
