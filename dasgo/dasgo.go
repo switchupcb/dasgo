6411700b9cc391e89dd3d5dcadaad7ee5635f7ab
switchupcb <contact@switchupcb.com>
2022-12-01 18:07:54 -0600

    add pointer helper functions

--- dasgo/dasgo.go
+++ dasgo/dasgo.go
@@ -27,3 +27,60 @@ type Value interface{}
 func Pointer[T any](v T) *T {
 	return &v
 }
+
+// IsValue returns whether the given pointer contains a value.
+func IsValue[T any](p *T) bool {
+	return p != nil
+}
+
+// PointerIndicator represents a Dasgo double pointer value indicator.
+type PointerIndicator uint8
+
+const (
+	// IsValueNothing indicates that the field was not provided.
+	//
+	// The double pointer is nil.
+	IsValueNothing PointerIndicator = 0
+
+	// IsValueNull indicates the the field was provided with a null value.
+	//
+	// The double pointer points to a nil pointer.
+	IsValueNull PointerIndicator = 1
+
+	// IsValueValid indicates that the field is a valid value.
+	//
+	// The double pointer points to a pointer that points to a value.
+	IsValueValid PointerIndicator = 2
+)
+
+// Pointer2 returns a double pointer to the given value.
+//
+// set `null` to true in order to point the double pointer to a `nil` pointer.
+func Pointer2[T any](v T, null ...bool) **T {
+	if len(null) != 0 && null[0] {
+		return new(*T)
+	}
+
+	pointer := Pointer(v)
+
+	return &pointer
+}
+
+// IsValue2 returns whether the given double pointer contains a value.
+//
+// returns IsValueNothing, IsValueNull, or IsValueValid.
+//
+// 	IsValueNothing indicates that the field was not provided.
+// 	IsValueNull indicates the the field was provided with a null value.
+// 	IsValueValid indicates that the field is a valid value.
+func IsValue2[T any](dp **T) PointerIndicator {
+	if dp != nil {
+		if *dp != nil {
+			return IsValueValid
+		}
+
+		return IsValueNull
+	}
+
+	return IsValueNothing
+}
